33b7c461369bd4b56fe5ba77baf9db83156f5b2b
Jayesh Poduval <jayeshpoduval31@gmail.com>
2024-07-23 00:05:04 +1000

    Code formatted

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import './App.css';
-import {useState} from 'react'
-import {motion, AnimatePresence} from 'framer-motion'
-import Tabs from './Components/Tabs';
-import { tabs } from './tabsData';
-import { toolbars } from './toolbarData';
-import Toolbar from './Components/Toolbar';
+import "./App.css";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Tabs from "./Components/Tabs";
+import { tabs } from "./tabsData";
+import { toolbars } from "./toolbarData";
+import Toolbar from "./Components/Toolbar";
 
 function App() {
-
-  const [activeTab, setActiveTab] = useState(tabs[0].id)
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
 
   return (
-    <div className='action-toolbar'>
+    <div className="action-toolbar">
       <header>
-        <main className='page-main'>
-          <section className='section'>
-            <div className='container'>
-              <Toolbar activeTab={activeTab}
-              setActiveTab={setActiveTab}   />
-              <Tabs activeTab={activeTab}
-              setActiveTab={setActiveTab} 
-              />
+        <main className="page-main">
+          <section className="section">
+            <div className="container">
+              <Toolbar activeTab={activeTab} setActiveTab={setActiveTab} />
+              <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
             </div>
           </section>
         </main>
